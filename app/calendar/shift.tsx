af729ba7816f23ff40697d5b7bc7ca78cf831adf
Jeppe Hauman <jeppe_hauman@hotmail.com>
2023-04-05 12:59:56 +0200

    confirm deletion and spinner

--- app/calendar/shift.tsx
+++ app/calendar/shift.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { BsXSquare } from "react-icons/bs";
+import { BsXSquare, BsCheckLg, BsXLg, BsX } from "react-icons/bs";
+import { FiCheck } from "react-icons/fi";
+import { Bars } from "react-loader-spinner";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
@@ -13,30 +15,71 @@ interface Props {
 
 const Shift = ({ employeeName, timeStart, timeEnd, shiftId }: Props) => {
   const [deleteHover, setDeleteHover] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleDelete = async () => {
+    setDeleteModal(false);
+    setLoading(true);
     const res = await fetch(`/api/shifts/${shiftId}`, {
       method: "DELETE",
       headers: {
         "Content-type": "application/json",
       },
     });
-    console.log(res);
 
     router.refresh();
+    setLoading(false);
   };
 
   return (
     <div
       onMouseEnter={() => setDeleteHover(true)}
       onMouseLeave={() => setDeleteHover(false)}
-      className="justify-between p-4 hover:cursor-pointer hover:bg-opacity-80 bg-purple-600 relative"
+      className="justify-between p-4 hover:bg-opacity-80 bg-purple-600 relative"
     >
+      {loading && (
+        <div
+          className={`w-full flex justify-center bg-purple-600 items-center mb-2 absolute top-0 left-0 h-full`}
+        >
+          <Bars
+            height="40"
+            width="40"
+            color="#fff"
+            ariaLabel="bars-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      )}
       {deleteHover && (
         <button className="absolute top-1 right-2 hover:text-white">
-          <BsXSquare onClick={handleDelete} className="stroke-1" />
+          <BsXSquare
+            onClick={() => setDeleteModal(true)}
+            className="stroke-1"
+          />
         </button>
+      )}
+      {deleteModal && (
+        <div className="absolute w-full h-full top-0 left-0 bg-black z-1000">
+          <h4 className="mb-4 text-xl">Are you sure?</h4>
+          <div className="flex justify-center gap-4">
+            <button
+              onClick={handleDelete}
+              className="h-8 w-14 hover border rounded-sm bg-neutral-500"
+            >
+              <FiCheck size="20" className="mx-auto" />
+            </button>
+            <button
+              onClick={() => setDeleteModal(false)}
+              className="h-8 w-14 hover border rounded-sm bg-neutral-500"
+            >
+              <BsXLg className="stroke-[.5] mx-auto" />
+            </button>
+          </div>
+        </div>
       )}
       <h3>{employeeName}</h3>
       <div>
