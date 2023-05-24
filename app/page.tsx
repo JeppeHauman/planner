a410b87059f57d8da61fcbf5356e58f8ea38893d
Jeppe Hauman <jeppe_hauman@hotmail.com>
2023-05-24 15:09:10 +0200

    Move signin button under heading

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ import { SignedIn, SignedOut } from "@clerk/nextjs/app-beta/client";
 export default function Home() {
   return (
     <div className="h-full flex flex-col items-center justify-center overflow-x-hidden">
+      <h1 className="p-4 text-6xl text-center mt-6 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-200 to-emerald-500">
+        Welcome to Planner <br />
+        The only app you need for managing your business
+      </h1>
       <SignedOut>
         <SignInButton mode="modal">
           <button className=" border rounded-md border-neutral-200 py-1 px-3">
@@ -17,10 +21,6 @@ export default function Home() {
       <SignedIn>
         <UserButton></UserButton>
       </SignedIn>
-      <h1 className="p-4 text-6xl text-center mt-6 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-200 to-emerald-500">
-        Welcome to Planner <br />
-        The only app you need for managing your business
-      </h1>
     </div>
   );
 }
